a6cc4eada0a56d27890c4b75eb7ce38015f3b392
ahmed <ahmedkalash513@gmail.com>
2023-06-06 17:16:09 +0300

    Make the dump more readable

--- controllers/TestController.php
+++ controllers/TestController.php
@@ -10,7 +10,7 @@ class TestController extends \app\core\Controller\Controller
        //$this->sessionHandler->delete('Flash_1');
         //$this->sessionHandler->flash('Flash_1', 'Flash_2');
         $all = $this->sessionHandler->all();
-        dump($all);
+        dd($all);
 
         dump($this->sessionHandler->newFlashDataKeys());
         dump($this->sessionHandler->oldFlashDataKeys());
